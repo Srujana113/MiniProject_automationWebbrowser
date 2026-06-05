8fa6bb9f0393b9e5336ce99fb6f2cd17794bd7ec
Achalla V S A Srujana <111237942+Srujana113@users.noreply.github.com>
2026-06-05 14:40:09 +0530

    Create CartPage.java

---
+++ src/main/java/com/srujana/pages/CartPage.java
@@ -0,0 +1,22 @@
+package com.srujana.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage extends BasePage {
+
+    private final By checkoutButton = By.id("checkout");
+    private final By cartItem       = By.className("cart_item");
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean hasItems() {
+        return isDisplayed(cartItem);
+    }
+
+    public void clickCheckout() {
+        click(checkoutButton);
+    }
+}
